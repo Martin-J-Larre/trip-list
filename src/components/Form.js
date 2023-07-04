648bdcf2af91f6dc0d3ddbcd5242df2c9e9784e2
Martin-J-Larre <martinjlarre@gmail.com>
2023-07-04 00:05:07 -0400

    #3 handle form, imput and select added

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,47 @@
-import React from "react";
+import { useState } from "react";
+
+const selectQuantity = Array.from({ length: 20 }, (_, i) => i + 1);
+const idGenerator = () => {
+  return Math.trunc(Math.random()).toString(36) + Date.now().toString(36);
+};
 
 export const Form = () => {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  const handleInput = ({ target }) => {
+    setDescription(target.value);
+  };
+
+  const handleSelect = ({ target }) => {
+    setQuantity(Number(target.value));
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: idGenerator() };
+    setDescription("");
+    setQuantity(1);
   };
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do you need for your Trip?</h3>
-      <select>
-        {[
-          1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-        ].map((num) => (
+      <select value={quantity} onChange={handleSelect}>
+        {selectQuantity.map((num) => (
           <option key={num} value={num}>
             {num}
           </option>
         ))}
       </select>
-      <input type="text" placeholder="Item..." />
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={handleInput}
+      />
       <button>Add</button>
     </form>
   );
